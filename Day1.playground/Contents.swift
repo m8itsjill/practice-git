3f494502e71d755889897e03d07c566d4292e349
haileysmallwood <hpkpixie@bellsouth.net>
2019-07-08 14:54:09 -0700

    First Save

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,116 @@
+import UIKit
+
+var str = "Hello, playground"
+var age = 14
+"I am \(age) years old"
+let string1 = "hello"
+let string2 = "there"
+var welcome = "\(string1) \(string2)"
+var anotherwelcome = string1 + " " + string2
+print ("Hello, World")
+var first = "Karlie"
+var last = "Kloss"
+" \(first)\(last)"
+" \(last)\(first)"
+" \(first) \(last)"
+" \(last) \(first) \(last) \(first)"
+" I love \(first)"
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+(a + b + c + d) / 4
+a + b * c / d
+(a + b) * c / d
+var integer = 20
+var double = 10.25
+Double (20) * double
+var celebrity = "Jimmy Kimmel"
+" Happy Birthday \(celebrity)"
+5 < 3
+12 > 7
+6 != 8
+7 == 7
+
+"karlie" == "karlie"
+"karlie" == " karlie"
+"Karlie" == "karlie"
+
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+
+var dogAge = 1
+if dogAge < 2 {
+    print("You are a puppy")
+}
+
+if dogAge == 2 {
+    print("You are awesome!")
+}
+
+if dogAge == 3 {
+    print("You are awesome!")
+}
+
+if dogAge == 4 {
+    print("You are awesome!")
+}
+
+else if dogAge > 12 {
+    print("You are elderly")
+}
+
+else {
+    print("You are also awesome.")
+}
+
+var favoriteFood = "Chicken Strips"
+
+if favoriteFood == "Chipotle" {
+    print("not a favorite")
+}
+
+else if favoriteFood == "Starbucks" {
+    print("coffee")
+}
+
+if favoriteFood == "Chicken Strips" {
+    print("yay")
+}
+
+else {
+    print("Food is yummy")
+}
+
+var hasAccount = false
+
+if hasAccount == false {
+print("Please log in!")
+}
+
+if hasAccount == true {
+    print("Yay you are in!")
+}
+
+var gradelevel = 5
+
+ if gradelevel <= 5 {
+    print("You are in elementary")
+}
+
+else if gradelevel <= 8 {
+    print("You are in middle school")
+}
+
+else if gradelevel <= 12 {
+    print("You are in high school")
+}
+
+else if gradelevel <= 16 {
+    print("You are in college")
+}
+
+ else {
+    print("what")
+}
